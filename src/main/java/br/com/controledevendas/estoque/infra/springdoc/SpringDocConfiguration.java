0478028b2ba6f4f246f6f5e1298877631bb9c9f2
Wesley D. Silva <wesleydsilva96@gmail.com>
2025-06-26 16:04:10 -0300

    implementado swagger da api

---
+++ src/main/java/br/com/controledevendas/estoque/infra/springdoc/SpringDocConfiguration.java
@@ -0,0 +1,19 @@
+package br.com.controledevendas.estoque.infra.springdoc;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("Controle De Estoque Api").version("1.0")
+                .description("api criada para manter o  controle de estoque")
+                .contact(new Contact().name("Wesley").email("wesleydsilva96@gmail.com")));
+    }
+}
